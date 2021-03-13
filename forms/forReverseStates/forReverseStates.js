f68d1a3f36ba226d421884d2c6b02ccf4ed2ca42
lewisdecarolis <lsd81882@creighton.edu>
2021-03-13 14:19:48 -0600

    reverse

--- forms/forReverseStates/forReverseStates.js
+++ forms/forReverseStates/forReverseStates.js
@@ -1,12 +1,4 @@
 let states = ["Nebraska", "Iowa", "Minnesota", "South Dakota", "North Dakota", "Kansas", "Oklahoma", "Texas"] 
 
-
-
-function reverseStates(states) {
-  var newArray = [];
-  for (var i = states.length - 1; i >= 0; i--) {
-    newArray.push(states[i]);
-  }
-  return newArray;
-  }
-console.log(reverseStates)
+for(i = 0; i < states.length; i++)
+    console.log(states.reverse([i]))
